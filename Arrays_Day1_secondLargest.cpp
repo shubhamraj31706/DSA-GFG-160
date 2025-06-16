eb5082125d7946ee79a829b5ee4a0319be13a7cd
Shubham Raj <sr31062006@gmail.com>
2025-06-16 10:35:40 +0530

    Added Day 1 - Second Largest Element

--- Arrays_Day1_secondLargest.cpp
+++ Arrays_Day1_secondLargest.cpp
@@ -1,3 +1,31 @@
+/*
+Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+Note: The second largest element should not be equal to the largest element.
+
+Examples:
+
+Input: arr[] = [12, 35, 1, 10, 34, 1]
+Output: 34
+Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+Input: arr[] = [10, 5, 10]
+Output: 5
+Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+Input: arr[] = [10, 10, 10]
+Output: -1
+Explanation: The largest element of the array is 10 and the second largest element does not exist.
+
+Expected Complexities
+    Time Complexity: O(n)
+    Auxiliary Space: O(1)
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
 int getSecondLargest(vector<int>& arr) {
     int largest = -1, secondLargest = -1;
     for(int i = 0; i < arr.size(); i++) {
