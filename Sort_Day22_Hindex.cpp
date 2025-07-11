86b590b3172c1243f24f7da6278144a696182884
Shubham Raj <sr31062006@gmail.com>
2025-07-11 09:40:08 +0530

    Added day 22 - Find H Index

---
+++ Sort_Day22_Hindex.cpp
@@ -0,0 +1,61 @@
+/*
+Given an integer array citations[], where citations[i] is the number of citations a researcher received for the ith paper. The task is to find the H-index.
+
+H-Index is the largest value such that the researcher has at least H papers that have been cited at least H times.
+
+Examples:
+
+Input: citations[] = [3, 0, 5, 3, 0]
+Output: 3
+Explanation: There are at least 3 papers (3, 5, 3) with at least 3 citations.
+
+Input: citations[] = [5, 1, 2, 4, 1]
+Output: 2
+Explanation: There are 3 papers (with citation counts of 5, 2, and 4) that have 2 or more citations. However, the H-Index cannot be 3 because there aren't 3 papers with 3 or more citations.
+
+Input: citations[] = [0, 0]
+Output: 0
+
+Expected Complexities
+    Time Complexity: O(n)
+    Auxiliary Space: O(n)
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int hIndex(vector<int>& citations) {
+    int n = citations.size();
+    vector<int> bucket(n + 1, 0);
+    
+    for (int i = 0; i < n; i++) {
+        if (citations[i] > n) {
+            bucket[n]++;
+        } else {
+            bucket[citations[i]]++;
+        }
+    }
+    
+    int count = 0;
+    for (int i = n; i >= 0; i--) {
+        count += bucket[i];
+        if (count >= i) {
+            return i;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    vector<int> citations1 = {3, 0, 5, 3, 0};
+    cout << hIndex(citations1) << endl;
+
+    vector<int> citations2 = {5, 1, 2, 4, 1};
+    cout << hIndex(citations2) << endl;
+
+    vector<int> citations3 = {0, 0};
+    cout << hIndex(citations3) << endl;
+
+    return 0;
+}
